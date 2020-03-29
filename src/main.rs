12750821c3ae99b563156513907d5abe567f9cca
armer song <armer_song@qq.com>
2020-03-29 11:34:35 +0800

    [Update]

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@
 extern crate libc;
 mod m;
 
-use core::slice;
-
 #[no_mangle]
 pub extern "C" fn main(argc: isize, argv: *const *const u8) -> isize {
-    let args = unsafe { slice::from_raw_parts(argv, argc as usize) };
     for i in 0..(argc as usize) {
         unsafe {
-            libc::printf("arg #%d: %s\n\0".as_ptr() as *const _, i, args[i]);
+            libc::printf("arg #%d: %s\n\0".as_ptr() as *const _, i, *argv.offset(i as isize));
         }
     }
+    
     for i in 0..4 {
         libc_println!("#{} this is a {}", i, "book");
     }
